442fae171e21218ff79c8f10690e5169b4feed4a
Diego Urrego <diegourrg@gmail.com>
2022-06-30 00:36:34 -0500

    Se realiza la búsqueda por héroe dependiendo del nombre ingresado, aún no está terminada. Se inataló la librería query-string y se empezó a usar el useLocation().

--- src/heroes/pages/SearchPage.jsx
+++ src/heroes/pages/SearchPage.jsx
@@ -1,6 +1,25 @@
-import { HeroCard } from "../components/HeroCard";
+import { useLocation, useNavigate } from 'react-router-dom';
+import queryString from 'query-string';
+import { useForm } from '../../hooks/useForm';
+import { HeroCard } from '../components/HeroCard';
+import { getHeroesByName } from '../helpers/getHeroesByName';
 
 export const SearchPage = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const { q = '' } = queryString.parse(location.search);
+  const heroes = getHeroesByName(q);
+  const { searchText, onInputChange } = useForm({
+    searchText: q,
+  });
+
+  const onSearchSubmit = (event) => {
+    event.preventDefault();
+    if (searchText.trim().length <= 1) return;
+    navigate(`?q=${searchText}`);
+  };
+
   return (
     <>
       <h1>Search</h1>
@@ -9,13 +28,15 @@ export const SearchPage = () => {
         <div className='col-5'>
           <h4>Searching</h4>
           <hr />
-          <form>
+          <form onSubmit={onSearchSubmit}>
             <input
               type='text'
               placeholder='Search a hero'
               className='form-control'
               name='searchText'
               autoComplete='off'
+              value={searchText}
+              onChange={onInputChange}
             />
             <button className='btn btn-outline-info mt-3'>Search</button>
           </form>
@@ -24,8 +45,12 @@ export const SearchPage = () => {
           <h4>Results</h4>
           <hr />
           <div className='alert alert-primary'>Search a hero</div>
-          <div className='alert alert-danger'><b>No hero found</b></div>
-          {/* <HeroCard /> */}
+          <div className='alert alert-danger'>
+            No hero found with <b>{q}</b>
+          </div>
+          {heroes.map((hero) => (
+            <HeroCard key={hero.id} {...hero} />
+          ))}
         </div>
       </div>
     </>
