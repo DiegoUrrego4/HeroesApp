d45de3cf93e59445d1f4ef02228ba10b5440f3f1
Diego Urrego <diegourrg@gmail.com>
2022-06-30 17:23:49 -0500

    Se crea el contexto general de la aplicación junto con su respectivo reducer para el caso en que el usuario esté loggeado.

--- src/auth/context/AuthProvider.jsx
+++ src/auth/context/AuthProvider.jsx
@@ -1,17 +1,29 @@
-import { useReducer } from "react"
-import { AuthContext } from "./AuthContext"
-import { authReducer } from "./authReducer"
+import { useReducer } from 'react';
+import { AuthContext } from './AuthContext';
+import { authReducer } from './authReducer';
+import { types } from '../types/types';
 
 const initialstate = {
-    logged: false
-}
+  logged: false,
+};
 
-export const AuthProvider = ({children}) => {
-    const [authState, dispatch] = useReducer(authReducer, initialstate)
-    
+export const AuthProvider = ({ children }) => {
+  const [authState, dispatch] = useReducer(authReducer, initialstate);
+
+  const login = (name = '') => {
+    const action = {
+      type: types.login,
+      payload: {
+        id: 'ABC',
+        name: name,
+      },
+    };
+    dispatch(action);
+  };
+
   return (
-    <AuthContext.Provider value={{}}>
-        {children}
+    <AuthContext.Provider value={{ ...authState, login: login }}>
+      {children}
     </AuthContext.Provider>
-  )
-}
+  );
+};
