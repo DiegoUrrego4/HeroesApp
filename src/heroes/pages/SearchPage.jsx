96718341e2cdf40f35611aa47ebdd14ee2883f14
Diego Urrego <diegourrg@gmail.com>
2022-06-30 00:54:08 -0500

    Se finaliza desarrollo del componente <SearchPage />

--- src/heroes/pages/SearchPage.jsx
+++ src/heroes/pages/SearchPage.jsx
@@ -10,13 +10,16 @@ export const SearchPage = () => {
 
   const { q = '' } = queryString.parse(location.search);
   const heroes = getHeroesByName(q);
+
+  const showSearch = q.length === 0;
+  const showError = q.length > 0 && heroes.length === 0;
+
   const { searchText, onInputChange } = useForm({
     searchText: q,
   });
 
   const onSearchSubmit = (event) => {
     event.preventDefault();
-    if (searchText.trim().length <= 1) return;
     navigate(`?q=${searchText}`);
   };
 
@@ -44,10 +47,20 @@ export const SearchPage = () => {
         <div className='col-7'>
           <h4>Results</h4>
           <hr />
-          <div className='alert alert-primary'>Search a hero</div>
-          <div className='alert alert-danger'>
+
+          <div
+            className='alert alert-primary animate__animated animate__fadeIn'
+            style={{ display: showSearch ? '' : 'none' }}
+          >
+            Search a hero
+          </div>
+          <div
+            className='alert alert-danger animate__animated animate__fadeIn'
+            style={{ display: showError ? '' : 'none' }}
+          >
             No hero found with <b>{q}</b>
           </div>
+
           {heroes.map((hero) => (
             <HeroCard key={hero.id} {...hero} />
           ))}
